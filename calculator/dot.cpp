e9afc4ec0b4986813a2a8a6ccb26ae58b1bb5321
VyacheslavPesherov <frobbyword@yandex.ru>
2019-06-30 15:32:43 +0700

    ALL START BEGIN

--- calculator/dot.cpp
+++
@@ -1,9 +0,0 @@
-#include "calculator.h"
-#include "ui_calculator.h"
-void Calculator::on_pushButton_dot_clicked()
-{
-    if(!(ui->results->text().contains('.')))
-     ui->results->setText(ui->results->text()+".");
-}
-
-
